08183cfe032eaa3ea884904aaf2b6ad82b0f301e
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2026-03-10 20:55:06 -0300

    Solve DNA

--- src/scripts/dna.py
+++ src/scripts/dna.py
@@ -6,7 +6,7 @@ from src.utils.dna_tools import count_nucleotides
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def solve():
-    with open(r"data\rosalind_dna (4).txt", "r") as f:
+    with open(r"data\rosalind_dna (5).txt", "r") as f:
         seq = f.read().strip()
         count_nucleotides(seq)
         
--- src/utils/dna_tools.py
+++ src/utils/dna_tools.py
@@ -1,4 +1,3 @@
-
 def validate_dna_string(dna_string: str) -> bool:
     if not isinstance(dna_string, str):
         return False
@@ -6,13 +5,14 @@ def validate_dna_string(dna_string: str) -> bool:
     if not dna_string:
         return False
     
-    valid_nucleotides = set('ATCG')
-    return all(nucleotide in valid_nucleotides for nucleotide in dna_string.upper())
+    valid_nucleodites = set('ATCG')
+    return all(nucleotide in valid_nucleodites for nucleotide in dna_string.upper())
+
 
 def count_nucleotides(dna_string: str) -> str:
     
     if not validate_dna_string(dna_string):
-        raise ValueError("Error: The sequence is NOT a valide DNA string!!!!!")
+        raise ValueError("Error: The sequence is NOT a valid DNA string!!!!!")
     
     N_A = dna_string.count("A")
     N_C = dna_string.count("C")
